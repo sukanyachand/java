1bf1d6743f5fefa5035306c39a09934a68874dfa
sukanyachand <sukanya9791@gmail.com>
2016-08-06 19:23:54 +0530

    Session1 Assignment1
    Session1 Assignment1

---
+++ SumNumber.java
@@ -0,0 +1,16 @@
+
+package sumnumber;
+
+public class SumNumber {
+
+    public static void main(String[] args) {
+        // Declaring and initializing variables with sum of two numbers
+        //session1 assignment 1
+        byte num1 = 18,num2 = 14;
+        int sum;       
+        sum = num1 + num2;  
+        System.out.println("Sum is :"+sum);
+        
+    }
+    
+}
